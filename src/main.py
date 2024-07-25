d82f17cb75a6fa3b1a6b72b87f20e9ca1734d75d
DSHakirov <shakirovdominatus@gmail.com>
2024-07-25 10:39:39 +0300

    fix

--- src/main.py
+++ src/main.py
@@ -1,12 +1,10 @@
 from dispatcher import bot
 from bot_logger import init_logger
-from settings import CRM_TOKEN
 
 
 logger = init_logger(__name__)
 
 
 if __name__ == "__main__":
-    print(CRM_TOKEN)
     print("Polling has started")
     bot.run_forever()
